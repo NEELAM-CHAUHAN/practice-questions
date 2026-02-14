84839550fa3d7a78a4150b529ecc6e3dd432ca81
neelam <ncnc12899@gmail.com>
2026-02-14 23:32:43 +0530

    Initial commit for JavaScript projects

--- patterns.cpp
+++ patterns.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 using namespace std;
 void printPattern(int n){
-  for (int i = 1; i < n; i++){
+  for (int i = 1; i < 2*n -1; i++){
+    int star = i;
+    if(star > n) star = n --1
     for (int j = 1; j <= n - i; j++)
-      cout << " ";
-    for (int k = 1; k <= 2 * i - 1; k++){
-        cout << "*";
-    }
+      cout << "*";
     cout << endl;
     }
 }    
